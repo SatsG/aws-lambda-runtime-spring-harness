a6781612789ac5b0f94172e82d1e3e46f9972d00
Sathya Ghelani <sathyaghelani@gmail.com>
2021-07-04 10:22:54 +0100

    Pull standard web flux config to be able to look up handlers

--- src/main/java/io/github/satsg/aws/lambda/runtime/spring/harness/config/EventHttpHandlerAutoConfiguration.java
+++ src/main/java/io/github/satsg/aws/lambda/runtime/spring/harness/config/EventHttpHandlerAutoConfiguration.java
@@ -1,12 +1,12 @@
 package io.github.satsg.aws.lambda.runtime.spring.harness.config;
 
+import java.util.Objects;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.web.reactive.DispatcherHandler;
+import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 
 @ConditionalOnProperty(
@@ -15,23 +15,12 @@ import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
       "satsg.enable.aws.lambda.runtime.reactive"
     })
 @Configuration
-public class EventHttpHandlerAutoConfiguration {
-
-  private final ApplicationContext applicationContext;
-
-  public EventHttpHandlerAutoConfiguration(ApplicationContext applicationContext) {
-    this.applicationContext = applicationContext;
-  }
+public class EventHttpHandlerAutoConfiguration extends WebFluxConfigurationSupport {
 
   @Bean
   @ConditionalOnMissingBean(HttpHandler.class)
   public HttpHandler httpHandler() {
-    return WebHttpHandlerBuilder.applicationContext(this.applicationContext).build();
-  }
-
-  @Bean
-  @ConditionalOnMissingBean(DispatcherHandler.class)
-  public DispatcherHandler webHandler() {
-    return new DispatcherHandler();
+    return WebHttpHandlerBuilder.applicationContext(Objects.requireNonNull(getApplicationContext()))
+        .build();
   }
 }
